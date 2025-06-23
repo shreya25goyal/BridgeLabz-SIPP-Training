388beba3047f75641d160ca0824e7b2792b06946
Shreya Goyal <goyalshreya2525@gmail.com>
2025-06-23 12:20:44 +0530

    Cleaned master branch to start fresh

--- JAVA Fundamentals/KilometerToMiles.java
+++
@@ -1,7 +0,0 @@
-public class KilometerToMiles {
-    public static void main(String[] args) {
-       double distancekm = 10.8;
-       double Miles = 10.8 / 1.6;
-       System.out.println("The distance 10.8 km in miles is :" + Miles);
-    }
-} 
